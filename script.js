4dad707ce3c3618b27259f9f86057b1f61ddb407
Davi Silva Santos <morg.daviss@gmail.com>
2022-02-22 21:38:43 -0300

    learned the types of selectors

---
+++ script.js
@@ -0,0 +1,11 @@
+/*let titulos = document.querySelector("[title]");
+
+console.log(titulos);*/
+
+/*let titulos = document.querySelectorAll("[title]");
+
+console.log(titulos);*/
+
+let titulos = document.querySelectorAll("[title*=tu]");
+
+console.log(titulos);
